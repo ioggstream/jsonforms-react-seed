e73b83f95f2ec6672580b881d0b59382edd4b24f
Roberto Polli <robipolli@gmail.com>
2020-06-25 00:01:44 +0200

    stub ghp

---
+++ precache-manifest.257dab5465c5fa964974a6d50d728c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc983ce8ee7fe6b4aca5559568fe1a22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f27a8a082f0a195a6d1",
+    "url": "/static/css/main.faaa6def.chunk.css"
+  },
+  {
+    "revision": "20f76e6a1fc40065525a",
+    "url": "/static/js/2.771fc891.chunk.js"
+  },
+  {
+    "revision": "623753edfbe38f2171fcc00114263a6c",
+    "url": "/static/js/2.771fc891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f27a8a082f0a195a6d1",
+    "url": "/static/js/main.8a6e7202.chunk.js"
+  },
+  {
+    "revision": "d2065d3287f86c4a10a1",
+    "url": "/static/js/runtime-main.8f708d19.js"
+  }
+]);
